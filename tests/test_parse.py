e08514c21a6d0448e2af0b56caf4a85ba176aa8f
Maris <mau13@case.edu>
2024-01-14 17:47:22 -0500

    Minimal header parsing

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -3,18 +3,18 @@ import os
 from datetime import datetime
 from pytz import timezone
 import eclipse_capture as ec
+from eclipse_capture import EclipseCaptureFile
 
 def test_parse_file(datadir):
-    capture_file = ec.capture_file(os.path.join(datadir, "test_capture1.csv"))
+    capture_file = EclipseCaptureFile.load(os.path.join(datadir, "capture.csv"))
     assert capture_file
 
-    assert capture_file.location == "Cleveland, OH"
-    assert capture_file.start_time == datetime(2024, 4, 8, 15, 13, 45, tzinfo=timezone("EST"))
-    assert capture_file.end_time == datetime(2024, 4, 8, 15, 17, 45, tzinfo=timezone("EST"))
+    assert capture_file.info.start == datetime(2024, 4, 8, 15, 13, 45, tzinfo=timezone("EST"))
+    assert capture_file.info.end == datetime(2024, 4, 8, 15, 17, 45, tzinfo=timezone("EST"))
 
 def test_bad_header(datadir):
     with pytest.raises(Exception) as e:
-        capture_file = ec.capture_file(os.path.join(datadir, "capture_bad_header.csv"))
+        capture_file = EclipseCaptureFile.load(os.path.join(datadir, "capture_bad_header.csv"))
 
     assert e.match("Invalid header")
 
